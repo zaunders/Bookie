700e2d63170db5a1afaa781b1369b5f0a534b757
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-11-06 18:16:58 +0100

    Deleted middleware for now

--- middleware/_helpers/send-email.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-const config = require('config.json');
-
-module.exports = sendEmail;
-
-async function sendEmail({ to, subject, html, from = config.emailFrom }) {
-    const transporter = nodemailer.createTransport(config.smtpOptions);
-    await transporter.sendMail({ from, to, subject, html });
-}
