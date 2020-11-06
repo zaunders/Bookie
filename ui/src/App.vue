0020c110f6abc52bda1939ed0c38908b84412cbc
Guillem Cordoba <guillem.cordoba@gmail.com>
2020-11-06 19:18:24 +0100

    Added the profiles module

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <hod-profile-prompt>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </hod-profile-prompt>
 </template>
 
 <style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount("#app");
+
+import { ProfilesModule } from "@holochain-open-dev/profiles";
+import { setupClient } from "./client";
+
+async function initApp() {
+  const client = await setupClient(`ws://localhost:8888`);
+
+  const profilesModule = new ProfilesModule({ apolloClient: client });
+
+  await profilesModule.install();
+}
+
+initApp();
